6b1bf95eea803ed3ac775173b8482082465d7d4e
DvoRick <max_kravchuk@list.ru>
2025-11-23 01:05:45 +0300

    работа с функцией инвентарем персонажа через функцию кортеж

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+inventory = () #создаем кортеж
 
-word = "pizza"
+#рассмотрим его как условие кориежа (интвентаря)
+if not inventory:
+    print("Inventory is empty") # проверка есть ли что то в инвентаре, инвентарь пуст
+input("enter to exit")
 
+inventory = ("sword",
+             "shield",
+             "chain",
+             "heal potion") #заполним инвентарь новым кортежом
+print("\nShow inventory")
+print(inventory)
+
+for item in inventory:
+    print(item)
+
+input("enter to exit")
+
+# страница 124 учебника
